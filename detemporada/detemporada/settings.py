54bc99326cf834fd7496db833469d6e442dc4430
Ciaran O'Reilly <ciaran@oreilly.cat>
2017-08-31 09:38:02 +0200

    added static_root to settings

--- detemporada/detemporada/settings.py
+++ detemporada/detemporada/settings.py
@@ -142,7 +142,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'static').replace('\\', '/')
 STATIC_URL = '/static/'
 
 # Filer and easy_thumbnail settings
